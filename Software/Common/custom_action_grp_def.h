77ab52d4bcf73207542ea1cd4cf1d3a0c3aeb007
bigJRo <108679888+bigJRo@users.noreply.github.com>
2025-01-11 22:51:45 -0800

    Create custom_action_grp_def.h

---
+++ Software/Common/custom_action_grp_def.h
@@ -0,0 +1,38 @@
+#ifndef _CUSTOM_ACTION_GRP_DEF_H
+#define _CUSTOM_ACTION_GRP_DEF_H
+
+/***************************************************************************************
+   Define custom action group values
+****************************************************************************************/
+//Base Action Group Definitions
+const uint8_t ag1_base = 1;
+const uint8_t ag2_base = 2;
+const uint8_t ag3_base = 3;
+const uint8_t ag4_base = 4;
+const uint8_t ag5_base = 5;
+const uint8_t ag6_base = 6;
+const uint8_t ag7_base = 7;
+const uint8_t ag8_base = 8;
+const uint8_t ag9_base = 9;
+const uint8_t ag10_base = 10;
+const uint8_t solar_array_base = 11;
+const uint8_t antenna_base = 12;
+const uint8_t cargo_door_base = 13;
+const uint8_t radiator_base = 14;
+const uint8_t drogue_base = 15;
+const uint8_t parachute_base = 16;
+const uint8_t ladder_base = 17;
+const uint8_t drogue_cut_base = 18;
+const uint8_t les_base = 19;
+const uint8_t science1_base = 20;
+const uint8_t science2_base = 21;
+const uint8_t collectSci_base = 22;
+const uint8_t engine1_base = 23;
+const uint8_t engine2_base = 24;
+const uint8_t engineMode_base = 25;
+const uint8_t intake_base = 26;
+const uint8_t ctrlPt1_base = 27;
+const uint8_t ctrlPt2_base = 28;
+const uint8_t airbrake_base = 29;
+
+#endif  // _CUSTOM_ACTION_GRP_DEF_H
