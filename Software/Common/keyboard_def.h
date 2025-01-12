9847d853176f3314dce5e64203bc5b3cf6f16c10
bigJRo <108679888+bigJRo@users.noreply.github.com>
2025-01-11 22:50:02 -0800

    Create keyboard_def.h

---
+++ Software/Common/keyboard_def.h
@@ -0,0 +1,56 @@
+#ifndef _KEYBOARD_DEF_H
+#define _KEYBOARD_DEF_H
+
+/***************************************************************************************
+   Define virtual key codes - see: https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+****************************************************************************************/
+const int16_t BACKSPACE_KEY = 0x08;
+const int16_t SHIFT_KEY = 0x10;
+const int16_t CTRL_KEY = 0x11;
+const int16_t LSHIFT_KEY = 0xA0;
+const int16_t LCTRL_KEY = 0xA2;
+const int16_t LALT_KEY = 0xA4;
+const int16_t TAB_KEY = 0x09;
+const int16_t COMMA_KEY = 0xBC;
+const int16_t PERIOD_KEY = 0xBE;
+const int16_t RIGHT_BRACKET_KEY = 0xDD;
+const int16_t LEFT_BRACKET_KEY = 0xDB;
+const int16_t APOSTROPHE_KEY = 0xDE;
+const int16_t EQUAL_KEY = 0xBB;
+const int16_t HYPHEN_KEY = 0xBD;
+const int16_t SLASH_KEY = 0xBF;
+const int16_t ESC_KEY = 0x1B;
+const int16_t SPACE_KEY = 0x20;
+const int16_t F1_KEY = 0x70;
+const int16_t F2_KEY = 0x71;
+const int16_t F5_KEY = 0x74;
+const int16_t F9_KEY = 0x78;
+const int16_t F12_KEY = 0x7B;
+const int16_t A_KEY = 0x41;
+const int16_t B_KEY = 0x42;
+const int16_t C_KEY = 0x43;
+const int16_t D_KEY = 0x44;
+const int16_t E_KEY = 0x45;
+const int16_t F_KEY = 0x46;
+const int16_t G_KEY = 0x47;
+const int16_t H_KEY = 0x48;
+const int16_t I_KEY = 0x49;
+const int16_t J_KEY = 0x4A;
+const int16_t K_KEY = 0x4B;
+const int16_t L_KEY = 0x4C;
+const int16_t M_KEY = 0x4D;
+const int16_t N_KEY = 0x4E;
+const int16_t O_KEY = 0x4F;
+const int16_t P_KEY = 0x50;
+const int16_t Q_KEY = 0x51;
+const int16_t R_KEY = 0x52;
+const int16_t S_KEY = 0x53;
+const int16_t T_KEY = 0x54;
+const int16_t U_KEY = 0x55;
+const int16_t V_KEY = 0x56;
+const int16_t W_KEY = 0x57;
+const int16_t X_KEY = 0x58;
+const int16_t Y_KEY = 0x59;
+const int16_t Z_KEY = 0x5A;
+
+#endif  // _KEYBOARD_DEF_H
