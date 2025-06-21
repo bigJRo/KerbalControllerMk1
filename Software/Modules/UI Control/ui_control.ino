286f11f7e70c71570f0e340ac64e97fd24a23f9b
bigJRo <108679888+bigJRo@users.noreply.github.com>
2025-06-20 23:59:13 -0700

    Update ui_control.ino
    Updated code base to be used on board containing RGB neopixel entries for all boxes. Code also optimized with ChatGPT

--- Software/Modules/UI Control/ui_control.ino
+++ Software/Modules/UI Control/ui_control.ino
@@ -1,114 +1,157 @@
 /********************************************************************************************************************************
   UI Control Module for Kerbal Controller Mk1
-
-  References UntitledSpaceCraft module code written by CodapopKSP (https://github.com/CodapopKSP/UntitledSpaceCraft)
-
+  References UntitledSpaceCraft module code by CodapopKSP (https://github.com/CodapopKSP/UntitledSpaceCraft)
   Licensed under the GNU General Public License v3.0 (GPL-3.0).
-
-  Final code was written by Jason Rostoker for Jeb's Controller Works.
+  Final code by J. Rostoker for Jeb's Controller Works.
 ********************************************************************************************************************************/
-#include <Wire.h>     // i2c libary compatible with ATtiny816
-#include <ShiftIn.h>  // Input Shift Register Library
-#include <Bounce2.h>  // Button Debounce Library
+
+#include <Wire.h>          // I2C library
+#include <ShiftIn.h>       // Input Shift Register
+#include <tinyNeoPixel.h>  // NeoPixel library for megaAVR (e.g., ATtiny816)
 
 /***************************************************************************************
-   Pin Definitions
+  Pin Definitions (ATtiny816 w/ megaTinyCore Pin Mapping)
 ****************************************************************************************/
-#define SDA PIN_PB1  // SDA
-#define SCL PIN_PB0  // SCL
+#define SDA          9   // PB1
+#define SCL          8   // PB0
 
-// Final Board Pin Configuration
-#define INT_OUT PIN_PB3      // Output interrupt
-#define load PIN_PA4         // 74HC165 Input Load Pin
-#define clockEnable PIN_PB7  // 74HC165 Input Clock Enable Pin
-#define clockIn PIN_PA5      // 74HC165 Input Clock Input
-#define dataInPin PIN_PB6    // 74HC165 Input Data Input Pin
+#define INT_OUT      4   // PA4
+#define load         7   // PA7
+#define clockEnable  6   // PA6
+#define clockIn      13  // PB5
+#define dataInPin    5   // PA5
+#define neopixCmd    12  // PB4
 
-/*
-// Test Board Pin Configuration
-#define INT_OUT PIN_PB2       // Output interrupt
-#define load PIN_PA6          // 74HC165 Input Load Pin
-#define clockEnable PIN_PC1   // 74HC165 Input Clock Enable Pin
-#define clockIn PIN_PC2       // 74HC165 Input Clock Input
-#define dataInPin PIN_PC0     // 74HC165 Input Data Input Pin
-*/
+#define led_13       11  // PB3
+#define led_14       15  // PC2
+#define led_15       10  // PB2
+#define led_16       14  // PC1
 
 /***************************************************************************************
-   I2C Address Definitions
+  Definitions
 ****************************************************************************************/
-#define Panel_MOD 0x21
+#define panel_addr     0x20
+#define NUM_LEDS       12
+#define NUM_BUTTONS    16
+
+tinyNeoPixel leds = tinyNeoPixel(NUM_LEDS, neopixCmd, NEO_GRB);
+
+struct buttonPixel {
+  char cmd[16];
+  uint8_t r, g, b;
+};
+
+const buttonPixel pixel_Default = { "Default", 32, 32, 32 };
+
+const buttonPixel pixel_Array[NUM_LEDS] = {
+  { "Map Enable",     0, 255,   0 },
+  { "Map Cycle -",    0, 255,   0 },
+  { "Map Cycle +",    0, 255,   0 },
+  { "Nav Mode",       0, 255,   0 },
+  { "IVA",            0, 255,   0 },
+  { "Cycle Cam",      0, 255,   0 },
+  { "Cycle Ship -",   0, 255,   0 },
+  { "Cycle Ship +",   0, 255,   0 },
+  { "Reset Focus",    0, 255,   0 },
+  { "Screen Shot",    0,   0, 255 },
+  { "UI",          255, 191,   0 },
+  { "DEBUG",       255,   0,   0 }
+};
+
+const uint8_t discreteLEDs[4] = { led_13, led_14, led_15, led_16 };
+
+uint8_t button_bytes[2] = {0, 0};
+uint8_t x[NUM_BUTTONS] = {0};
+ShiftIn<2> shift;
+
+volatile bool updateLED = false;
+uint16_t led_bits = 0;
+uint16_t prev_led_bits = 0;
 
 /***************************************************************************************
-   Global definitions for input (control)shift registers (74HC165)
-****************************************************************************************/
-int16_t x[16] = {};
-uint8_t button_bytes[2] = {};
-ShiftIn<2> shift;  // Object for control shift registers
-
-
-/***************************************************************************************
-  Arduino Setup Function
+  Setup
 ****************************************************************************************/
 void setup() {
-  /********************************************************
-    Set Pin Modes for necessary inputs/outputs
-  *********************************************************/
   pinMode(INT_OUT, OUTPUT);
+  for (uint8_t i = 0; i < 4; i++) {
+    pinMode(discreteLEDs[i], OUTPUT);
+    digitalWrite(discreteLEDs[i], LOW);
+  }
+  digitalWrite(INT_OUT, HIGH);
 
-  /********************************************************
-    Set Output Interrupt Configuration
-  *********************************************************/
-  digitalWrite(INT_OUT, HIGH);  //Set INT_OUT to default state
-
-  /********************************************************
-    Start object for 74HC165 Input Shift register
-  *********************************************************/
   shift.begin(load, clockEnable, dataInPin, clockIn);
 
-  encButton.setPressedState(LOW);  // INDICATE THAT THE LOW STATE CORRESPONDS TO PHYSICALLY PRESSING THE BUTTON
+  leds.begin();
+  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+    leds.setPixelColor(i, pixel_Default.r, pixel_Default.g, pixel_Default.b);
+  }
+  leds.show();
 
-  /********************************************************
-    Begin I2C comm and register receive and request events
-  *********************************************************/
-  Wire.begin(SDA, SCL, Panel_MOD);  // new syntax: join i2c bus (address required for slave)
-  Wire.onReceive(receiveEvent);     // register event
-  Wire.onRequest(requestEvent);     // register event
+  Wire.begin(SDA, SCL, panel_addr);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
 }
 
-
 /***************************************************************************************
-  Arduino Loop Function
+  Loop
 ****************************************************************************************/
 void loop() {
-  /********************************************************
-    Update the Button configurations
-  *********************************************************/
-  if (shift.update()) {  // updates button input and returns value if pressed
-    for (uint8_t i = 0; i < 16; i++) {
-      x[i] = shift.state(i);  // get state of button i
+  if (updateLED) {
+    handle_ledUpdate();
+    updateLED = false;
+  }
+
+  if (shift.update()) {
+    for (uint8_t i = 0; i < NUM_BUTTONS; i++) {
+      x[i] = shift.state(i);
     }
 
-    // set the inputs bytes that can be used to LED display and transmit
-    for (int i = 0; i < 8; i++) {
-      bitWrite(button_bytes[0], i, x[i]);
-    }
-    for (int i = 0; i < 8; i++) {
-      bitWrite(button_bytes[1], i, x[i + 8]);
+    button_bytes[0] = 0;
+    button_bytes[1] = 0;
+    for (uint8_t i = 0; i < 8; i++) {
+      button_bytes[0] |= (x[i] & 1) << i;
+      button_bytes[1] |= (x[i + 8] & 1) << i;
     }
 
-    digitalWrite(INT_OUT, LOW);  //Set INT_OUT to indicate to MC there is a change
+    digitalWrite(INT_OUT, LOW);  // Notify master
   }
 }
 
+/***************************************************************************************
+  Handle LED Updates
+****************************************************************************************/
+void handle_ledUpdate() {
+  for (uint8_t i = 0; i < NUM_BUTTONS; i++) {
+    bool newState = bitRead(led_bits, i);
+    bool oldState = bitRead(prev_led_bits, i);
+
+    if (newState != oldState) {
+      if (i < NUM_LEDS) {
+        const buttonPixel& px = newState ? pixel_Array[i] : pixel_Default;
+        leds.setPixelColor(i, px.r, px.g, px.b);
+      } else if (i < NUM_BUTTONS) {
+        digitalWrite(discreteLEDs[i - NUM_LEDS], newState ? HIGH : LOW);
+      }
+    }
+  }
+
+  leds.show();
+  prev_led_bits = led_bits;
+}
 
 /***************************************************************************************
-   I2C Request Event
-   function that executes whenever data is requested by master
-    this function is registered as an event, see setup()
-   - No inputs
-   - No outputs
+  I2C Events
 ****************************************************************************************/
 void requestEvent() {
-  Wire.write(button_bytes[0]);  // respond with button message high byte
-  Wire.write(button_bytes[1]);  // respond with button message low byte
+  Wire.write(button_bytes[0]);
+  Wire.write(button_bytes[1]);
+}
+
+void receiveEvent(int howMany) {
+  if (Wire.available() >= 2) {
+    uint8_t lsb = Wire.read();
+    uint8_t msb = Wire.read();
+    led_bits = (msb << 8) | lsb;
+    updateLED = true;
+  }
 }
