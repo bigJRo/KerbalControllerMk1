2a248730b28e1e95926a845f1b47816d1b8ea601
bigJRo <108679888+bigJRo@users.noreply.github.com>
2025-06-27 15:16:25 -0700

    Update ExampleUsage.ino
    Updated to final core configuration

--- Software/Modules/Button Modules/ButtonModuleCore/examples/ExampleUsage/ExampleUsage.ino
+++ Software/Modules/Button Modules/ButtonModuleCore/examples/ExampleUsage/ExampleUsage.ino
@@ -1,12 +1,23 @@
 #include <ButtonModuleCore.h>
 
+constexpr uint8_t panel_addr = 0x23;
+
+const char commandNames[16][16] PROGMEM = {
+  "Cmd1", "Cmd2", "Cmd3", "Cmd4",
+  "Cmd5", "Cmd6", "Cmd7", "Cmd8",
+  "Cmd9", "Cmd10", "Cmd11", "Cmd12",
+  "Lock1", "Lock2", "Lock3", "Lock4"
+};
+
 void setup() {
-  ButtonModuleCore::initHardware();
-  ButtonModuleCore::debugBlink(LED_BUILTIN, 5);
+  beginModule(panel_addr);
 }
 
 void loop() {
-  uint16_t val = ButtonModuleCore::readStableAnalog(A0);
-  Serial.println(val);
-  delay(500);
+  if (updateLED) {
+    // Example: handle_ledUpdate();
+    updateLED = false;
+  }
+
+  readButtonStates();
 }
